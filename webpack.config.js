8645f34c21136031ed98aec7937c12ee49227245
ykohata <ykohata@appirio.com>
2016-03-23 01:06:51 +0900

    Login as connect user

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,13 @@ if (process.env.ENV == 'DEV') {
   process.env.COOKIE_DOAMIN      = '.topcoder-dev.com'
   process.env.LOGIN_RETURN_URL   = 'https://sample.topcoder-dev.com/'
   process.env.LOGOUT_RETURN_URL  = 'https://sample.topcoder-dev.com/'
-  process.env.ACCOUNTS_LOGIN_URL = 'https://accounts.topcoder-dev.com/login'
+  process.env.ACCOUNTS_LOGIN_URL = 'https://accounts.topcoder-dev.com/connect'
   process.env.ACCOUNTS_LOGOUT_URL= 'https://accounts.topcoder-dev.com/logout'
 } else {
   process.env.COOKIE_DOAMIN      = '.topcoder-dev.com'
   process.env.LOGIN_RETURN_URL   = 'http://local.sample.topcoder-dev.com:3100/'
   process.env.LOGOUT_RETURN_URL  = 'http://local.sample.topcoder-dev.com:3100/'
-  process.env.ACCOUNTS_LOGIN_URL = 'http://local.topcoder-dev.com:3000/login'
+  process.env.ACCOUNTS_LOGIN_URL = 'http://local.topcoder-dev.com:3000/connect'
   process.env.ACCOUNTS_LOGOUT_URL= 'http://local.topcoder-dev.com:3000/logout'
 }
 
